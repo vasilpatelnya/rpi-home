45853c96312a3e6870e9741c35420755ac9e2b66
Vasil Patelnya <vasilpatelnya@protonmail.com>
2020-08-08 16:27:08 +0300

    Не прошел тест на тревисе, смотрю что не совпало по кириллице.

--- internal/app/rpi-detector-mongo/detector_test.go
+++ internal/app/rpi-detector-mongo/detector_test.go
@@ -76,7 +76,7 @@ func TestEvent_GetVideoReadyMessage(t *testing.T) {
 		Created: time.Date(2020, 8, 1, 12, 30, 0, 0, time.UTC).UnixNano(),
 		Updated: time.Date(2020, 8, 1, 12, 30, 0, 0, time.UTC).UnixNano(),
 	}
-	assert.True(t, event.GetVideoReadyMessage() == "Видео от 1 августа 2020 15:30")
+	assert.Equal(t, event.GetVideoReadyMessage(), "Видео от 1 августа 2020 15:30")
 }
 
 func TestEvent_VideoReadyHandler(t *testing.T) {
