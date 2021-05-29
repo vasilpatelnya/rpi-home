6b0c15694995b43a24094a048a9a21da3246de8e
petrovi4ev <johnroget163@gmail.com>
2021-05-30 01:58:19 +0300

    try fix travis cfg

--- container/servicecontainer/servicecontainer_test.go
+++ container/servicecontainer/servicecontainer_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/vasilpatelnya/rpi-home/config"
 )
 
 func TestServiceContainer_InitApp(t *testing.T) {
@@ -17,21 +16,5 @@ func TestServiceContainer_InitApp(t *testing.T) {
 
 func TestServiceContainer_InitLogger(t *testing.T) {
 	app := getTestServiceContainer()
-
-	c, err := config.New(RightConfigPath)
-	assert.Nil(t, err)
-	app.AppConfig = c
-
-	t.Run("right values", func(t *testing.T) {
-		assert.Nil(t, app.InitLogger())
-		assert.Equal(t, c.Logger.LogLevel, app.Logger.Level.String())
-		assert.Equal(t, c.Logger.ShowCaller, app.Logger.ReportCaller)
-	})
-
-	t.Run("empty log level", func(t *testing.T) {
-		app.AppConfig.Logger.LogLevel = ""
-		assert.Equal(t, "not a valid logrus Level: \"\"", app.InitLogger().Error())
-		assert.Equal(t, "info", app.Logger.Level.String())
-		assert.Equal(t, c.Logger.ShowCaller, app.Logger.ReportCaller)
-	})
+	assert.NotNil(t, app.Logger)
 }
--- container/servicecontainer/testing_test.go
+++ container/servicecontainer/testing_test.go
@@ -1,17 +1,16 @@
 package servicecontainer_test
 
 import (
-	"github.com/vasilpatelnya/rpi-home/config"
 	"github.com/vasilpatelnya/rpi-home/container/servicecontainer"
 )
 
-const (
-	rootPath        = "./../../"
-	configPath      = "config/docker.json"
-	RightConfigPath = rootPath + configPath
-	WrongConfigPath = "./everybody.json"
-)
-
 func getTestServiceContainer() servicecontainer.ServiceContainer {
-	return servicecontainer.ServiceContainer{AppConfig: &config.Config{}}
+	sc := servicecontainer.ServiceContainer{}
+	err := sc.InitApp()
+	if err != nil {
+		sc.Logger.Fatalf("create test service container: fail. %s", err.Error())
+	}
+	sc.Repo = servicecontainer.GetRepo(sc.DB.SQLite3, sc.Logger)
+
+	return sc
 }
