8985db73b80ebc218fdb25e4e92691bf6a43de44
petrovi4ev <johnroget163@gmail.com>
2021-07-08 19:45:17 +0300

    fix

--- usecase/event_handler.go
+++ usecase/event_handler.go
@@ -14,9 +14,7 @@ import (
 )
 
 const (
-	MaxSize          int64 = 50 * 1024 * 1024 // 50mb
-	StatusUpdated          = 11
-	StatusNotUpdated       = -11
+	MaxSize int64 = 50 * 1024 * 1024 // 50mb
 )
 
 type EventHandleOpts struct {
@@ -30,11 +28,11 @@ type EventHandleOpts struct {
 }
 
 func EventHandle(opts EventHandleOpts) {
-	commonEventsHandler(model.StatusFail, opts)
-	commonEventsHandler(model.StatusNew, opts)
+	handler(model.StatusFail, opts)
+	handler(model.StatusNew, opts)
 }
 
-func commonEventsHandler(status int, opts EventHandleOpts) {
+func handler(status int, opts EventHandleOpts) {
 	events, err := opts.Repo.GetAllByStatus(status)
 	if err != nil {
 		sentryhelper.Handle(opts.Logger, err, "Ошибка получения записей событий из БД")
@@ -44,29 +42,15 @@ func commonEventsHandler(status int, opts EventHandleOpts) {
 		for _, e := range events {
 			switch e.Type {
 			case model.TypeMotion:
-				status, err := handleMotionAlarm(opts, &e)
+				err := handleAlarm(&e, opts)
 				if err != nil {
 					msg := fmt.Sprintf("Ошибка обработки события: %s %s", e.Name, err.Error())
 					sentryhelper.Handle(opts.Logger, err, msg)
-					if status == model.StatusNotSent {
-						e.Status = model.StatusFail
-						err = opts.Repo.Save(&e)
-						if err != nil {
-							msg := fmt.Sprintf("Ошибка сохранения события: %s %s", e.Name, err.Error())
-							sentryhelper.Handle(opts.Logger, err, msg)
-						}
-						continue
-					}
-				}
-				e.Status = model.StatusReady
-				err = opts.Repo.Save(&e)
-				if err != nil {
-					msg := fmt.Sprintf("Ошибка сохранения события: %s %s", e.Name, err.Error())
-					sentryhelper.Handle(opts.Logger, err, msg)
+
+					continue
 				}
 			case model.TypeMovieReady:
-				opts.Logger.Info("Видео готово!")
-				e.Status, err = handleMotionReady(&e, opts)
+				err = handleVideoReady(&e, opts)
 				if err != nil {
 					msg := fmt.Sprintf("Ошибка обработки события: %s %s", e.Name, err.Error())
 					sentryhelper.Handle(opts.Logger, err, msg)
@@ -82,31 +66,15 @@ func commonEventsHandler(status int, opts EventHandleOpts) {
 	}
 }
 
-func handleMotionAlarm(opts EventHandleOpts, e *model.Event) (int, error) {
+func handleVideoReady(e *model.Event, opts EventHandleOpts) error {
 	if !opts.UseNotifier {
-		return model.StatusNotSent, errors.New("отправка уведомлений запрещена")
-	}
-	err := opts.Notifier.SendText(e.GetMotionMessage())
-	if err != nil {
-		return model.StatusNotSent, errors.New("ошибка отправки текста о срабатывании")
-	}
-	err = opts.Repo.Save(e)
-	if err != nil {
-		return StatusNotUpdated, errors.New(fmt.Sprintf("ошибка обновления записи в БД, id записи: %s", e.ID.Hex()))
-	}
-
-	return StatusUpdated, nil
-}
-
-func handleMotionReady(e *model.Event, opts EventHandleOpts) (int, error) {
-	if !opts.UseNotifier {
-		return model.StatusNotSent, errors.New("отправка уведомлений запрещена")
+		return errors.New("отправка уведомлений запрещена")
 	}
 	l, err := fs.GetTodayFileList(opts.TargetDir, model.LayoutISO)
 	if err != nil {
 		opts.Logger.Errorf("Ошибка получения списка файлов в директории %s: %s", opts.TargetDir, err.Error())
 
-		return model.StatusNotSent, err
+		return err
 	}
 	for _, f := range l {
 		todayDir := fs.GetTodayDir(model.LayoutISO)
@@ -120,31 +88,49 @@ func handleMotionReady(e *model.Event, opts EventHandleOpts) (int, error) {
 				if err != nil {
 					opts.Logger.Errorf("Ошибка при попытке скопировать видео \nиз %s \nв %s\n Ошибка: %s", fp, dstPath, err.Error())
 
-					return model.StatusNotSent, err
+					return err
 				}
 				err = opts.Notifier.SendFile(fp, msg)
 				if err != nil {
 					opts.Logger.Errorf("Ошибка при попытке отправить видео %s: %s", f.Name(), err.Error())
 
-					return model.StatusNotSent, err
+					return err
 				}
 				opts.Logger.Infof("файл %s был отправлен в телеграм", fp)
 				err = os.Remove(fp)
 				if err != nil {
 					opts.Logger.Errorf("Ошибка при попытке удалить файл: %s: %s", f.Name(), err.Error())
 
-					return model.StatusNotSent, err
+					return err
 				}
 			} else {
 				err := opts.Notifier.SendText("Файл слишком велик чтобы его пересылать в Telegram. Вы можете его посмотреть через веб-интерфейс. Имя файла: " + f.Name())
 				sentryhelper.Handle(opts.Logger, err, "Не удалось отправить текстовое сообщение о превышении размера видеофайла.")
 				e.Status = model.StatusCanceled
-				if err := opts.Repo.Save(e); err != nil {
-					opts.Logger.Warningf("Ошибка сохранения события со статусом 'отменен'")
-				}
 			}
 		}
 	}
 
-	return model.StatusSent, nil
+	return opts.Repo.Save(e)
+}
+
+func handleAlarm(e *model.Event, opts EventHandleOpts) error {
+	if !opts.UseNotifier {
+		return errors.New("отправка уведомлений запрещена")
+	}
+	err := opts.Notifier.SendText(e.GetMotionMessage())
+	if err != nil {
+		err = opts.Repo.SaveUpdated(e, model.StatusFail)
+		if err != nil {
+			return errors.New(fmt.Sprintf("ошибка обновления записи в БД, id записи: %s [%s]", e.ID.Hex(), err.Error()))
+		}
+
+		return errors.New("ошибка отправки текста о срабатывании")
+	}
+	err = opts.Repo.SaveUpdated(e, model.StatusReady)
+	if err != nil {
+		return errors.New(fmt.Sprintf("ошибка обновления записи в БД, id записи: %s [%s]", e.ID.Hex(), err.Error()))
+	}
+
+	return nil
 }
